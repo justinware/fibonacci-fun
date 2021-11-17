ce0d07300f3e20f2366c158538b7bfb338ab8ef7
Justin Ware <wareisjustin@gmail.com>
2021-11-17 11:36:25 +1000

    Nov 2021 packages upgrade

--- src/console/messages.ts
+++ src/console/messages.ts
@@ -1,5 +1,5 @@
-import * as chalk from 'chalk';
-import * as figlet from 'figlet';
+import chalk, { Chalk } from 'chalk';
+import figlet from 'figlet';
 
 import { IUserInputData, IUserInputStats } from '../shared/types/IUserInputStore';
 
@@ -10,9 +10,15 @@ const colours = {
   red: 'red'
 };
 
+const chalkFuncMap = new Map<string, Chalk>();
+chalkFuncMap.set(colours.yellow, chalk.yellow);
+chalkFuncMap.set(colours.cyan, chalk.cyan);
+chalkFuncMap.set(colours.green, chalk.green);
+chalkFuncMap.set(colours.red, chalk.red);
+
 const write = (colour: string, message: string): void => {
 
-  console.log(chalk[colour](message));
+  console.log(chalkFuncMap.get(colour)(message));
 };
 
 export const updateDuration: string = 'Please input the number of time in seconds between emitting numbers and their frequency\n';
@@ -23,24 +29,24 @@ export const showBanner = (): void => {
 
   console.clear();
   write(colours.yellow, figlet.textSync('Fibonacci Fun', { horizontalLayout: 'full' }));
-  
+
   const message =
     `Author: Justin Ware\n
 This program repeatedly asks to input a command or number, checks if input number is in
-the 1st 1000 numbers of the Fibonacci sequence, and then displays input number frequency 
+the 1st 1000 numbers of the Fibonacci sequence, and then displays input number frequency
 statistics (counts) on a user-specified timer.
-    
+
 To use the program enter a number when prompted OR one of the following commands:
-    
-  ${chalk[colours.cyan]('halt')}: pause the updates of number statistics
-  ${chalk[colours.cyan]('resume')}: resume the updates of number statistics
-  ${chalk[colours.cyan]('quit')}: terminate the program\n`;
+
+  ${chalk.cyan('halt')}: pause the updates of number statistics
+  ${chalk.cyan('resume')}: resume the updates of number statistics
+  ${chalk.cyan('quit')}: terminate the program\n`;
 
   console.log(message);
 };
 
 export const showBadInput = (): void => {
-  
+
   write(colours.red, 'Bad input. Please enter a valid number or one of the commands: halt, resume or quit.');
 };
 
@@ -48,7 +54,7 @@ export const showStats = (stats: IUserInputStats) => {
 
   const statsText = stats.map((s: IUserInputData) => `${s.value}:${s.frequency}`)
                          .join(', ');
-  
+
   write(colours.green, statsText);
 };
 
--- test/shared/controllerSpec.ts
+++ test/shared/controllerSpec.ts
@@ -30,16 +30,16 @@ const stubNotifier: INotifier = {
 describe('Controller', () => {
 
   describe('#addNumber()', () => {
-              
+
     let stubStoreAdd: sinon.SinonStub<[number], IUserInputStore>;
     let stubDataSourceItemExists: sinon.SinonStub<[number], boolean>;
     let controller: IController;
-    
+
     before(() => {
 
       stubStoreAdd = sinon.stub(stubStore, 'add');
       stubDataSourceItemExists = sinon.stub(stubDataSource, 'itemExists').returns(true);
-      
+
       controller = new Controller(stubStore, stubDataSource, undefined);
     });
 
@@ -48,32 +48,32 @@ describe('Controller', () => {
       stubStoreAdd.restore();
       stubDataSourceItemExists.restore();
     });
-        
+
     it(`should add the number to the user input store`, () => {
-      
+
       controller.addNumber(11);
 
       expect(stubStoreAdd.calledOnceWithExactly(11)).to.be.true;
     });
 
     it(`should check if the number exists in the data source and return the result`, () => {
-      
+
       let result = controller.addNumber(11);
 
       expect(result).to.be.true;
-    });    
+    });
   });
 
   describe('#statsStream', () => {
-              
+
     const stubStats: IUserInputStats = [];
     let stubNotifierStats: sinon.SinonStub<any[], any>;
     let controller: IController;
-    
+
     before(() => {
 
       stubNotifierStats = sinon.stub(stubNotifier, 'statsStream').get(() => stubStats);
-      
+
       controller = new Controller(undefined, undefined, stubNotifier);
     });
 
@@ -81,24 +81,24 @@ describe('Controller', () => {
 
       stubNotifierStats.restore();
     });
-        
+
     it(`should return the statsStream from the notifier`, () => {
-      
+
       expect(controller.statsStream).to.deep.equal(stubStats);
     });
   });
 
   describe('#halt()', () => {
-              
+
     let stubNotifierIsPaused: sinon.SinonStub<any[], any>;
     let stubNotifierTogglePaused: sinon.SinonStub<[], void>;
     let controller: IController;
-    
+
     before(() => {
 
       stubNotifierIsPaused = sinon.stub(stubNotifier, 'isPaused').get(() => false);
       stubNotifierTogglePaused = sinon.stub(stubNotifier, 'togglePaused');
-      
+
       controller = new Controller(undefined, undefined, stubNotifier);
     });
 
@@ -107,26 +107,26 @@ describe('Controller', () => {
       stubNotifierIsPaused.restore();
       stubNotifierTogglePaused.restore();
     });
-        
+
     it(`should toggle the paused state on the notifier`, () => {
-      
+
       controller.halt();
-      
+
       expect(stubNotifierTogglePaused.calledOnce).to.be.true;
     });
   });
 
   describe('#resume()', () => {
-              
+
     let stubNotifierIsPaused: sinon.SinonStub<any[], any>;
     let stubNotifierTogglePaused: sinon.SinonStub<[], void>;
     let controller: IController;
-    
+
     before(() => {
 
       stubNotifierIsPaused = sinon.stub(stubNotifier, 'isPaused').get(() => true);
       stubNotifierTogglePaused = sinon.stub(stubNotifier, 'togglePaused');
-      
+
       controller = new Controller(undefined, undefined, stubNotifier);
     });
 
@@ -135,27 +135,27 @@ describe('Controller', () => {
       stubNotifierIsPaused.restore();
       stubNotifierTogglePaused.restore();
     });
-        
+
     it(`should toggle the paused state on the notifier`, () => {
-      
+
       controller.resume();
-      
+
       expect(stubNotifierTogglePaused.calledOnce).to.be.true;
     });
   });
 
   describe('#quit()', () => {
-              
+
     const stubStats: IUserInputStats = [];
     let stubNotifierFinalise: sinon.SinonStub<[], void>;
     let stubStoreStats: sinon.SinonStub<any[], any>;
     let controller: IController;
-    
+
     before(() => {
 
       stubNotifierFinalise = sinon.stub(stubNotifier, 'finalise');
       stubStoreStats = sinon.stub(stubStore, 'statistics').get(() => stubStats);
-      
+
       controller = new Controller(stubStore, undefined, stubNotifier);
     });
 
@@ -164,19 +164,19 @@ describe('Controller', () => {
       stubNotifierFinalise.restore();
       stubStoreStats.restore();
     });
-        
+
     it(`should call finalise on the notifier`, () => {
-      
+
       controller.quit();
 
       expect(stubNotifierFinalise.calledOnce).to.be.true;
     });
 
     it(`should return the statistics from the user input store`, () => {
-      
+
       let result = controller.quit();
 
       expect(result).to.deep.equal(stubStats);
-    });    
+    });
   });
 });
